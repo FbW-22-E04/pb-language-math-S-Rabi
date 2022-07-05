da09ce2d6c54a64fe585754edd95193141271a34
S Rabi <rabi.sanaye@yahoo.com>
2022-07-05 18:44:22 +0200

    last exercise

---
+++ solution.js
@@ -0,0 +1,21 @@
+console.log(Math.min(-1, 0, 1, 2, 3, 4));
+console.log(Math.max(-1, 0, 1, 2, 3, 4));
+
+// const numbers = [3321.32321, 326.76, 76788.7, -9.78, 43.342];
+// console.log();
+
+console.log(Math.ceil(3321.32321));
+console.log(Math.ceil(326.76));
+console.log(Math.ceil(76788.7));
+console.log(Math.ceil(-9.78));
+console.log(Math.ceil(43.342));
+//
+console.log(Math.floor(3321.32321));
+console.log(Math.floor(326.76));
+console.log(Math.floor(76788.7));
+console.log(Math.floor(-9.78));
+console.log(Math.floor(28.329));
+//
+const random = Math.random();
+// console.log(random);
+console.log(Math.ceil(random * 6));
